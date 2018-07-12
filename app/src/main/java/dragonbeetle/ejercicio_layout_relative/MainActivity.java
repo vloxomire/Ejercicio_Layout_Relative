8650a3569ed53c6b9e0c03ac335c764d2702960e
makusu <vloxomire@gmail.com>
2018-07-12 19:59:15 -0300

    botones java

--- app/src/main/java/dragonbeetle/ejercicio_layout_relative/MainActivity.java
+++ app/src/main/java/dragonbeetle/ejercicio_layout_relative/MainActivity.java
@@ -2,12 +2,16 @@ package dragonbeetle.ejercicio_layout_relative;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button boton1, boton2, boton3 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
